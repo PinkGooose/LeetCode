3c40724a33ded98dd314a560b4ee138f7e15e65e
Max <mister.kurnosenckoff@yandex.ru>
2025-10-22 15:04:35 +0300

    Add task addTwoNumbers

--- src/tasks/Task34.java
+++ src/tasks/Task34.java
@@ -3,30 +3,30 @@ package tasks;
 import java.util.Scanner;
 
 public class Task34 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int n = scanner.nextInt();
+//        int x = scanner.nextInt();
+//        int y = scanner.nextInt();
+//        int z = scanner.nextInt();
+//
+//        long[] numbers = new long[n];
+//        for (int i = 0; i < n; i++) {
+//            numbers[i] = scanner.nextLong();
+//        }
+//
+//        System.out.println(solveTask(n, x, y, z, numbers));
+//
+//        scanner.close();
+//    }
 
-        int n = scanner.nextInt();
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        int z = scanner.nextInt();
-
-        long[] numbers = new long[n];
-        for (int i = 0; i < n; i++) {
-            numbers[i] = scanner.nextLong();
-        }
-
-        System.out.println(solveTask(n, x, y, z, numbers));
-
-        scanner.close();
-    }
-
-    private static int solveTask(int n, int x, int y, int z, long[] numbers) {
-        int[] opersX = new int[n];
-        int[] opersY = new int[n];
-        int[] opersZ = new int[n];
-
-
-    }
+//    private static int solveTask(int n, int x, int y, int z, long[] numbers) {
+//        int[] opersX = new int[n];
+//        int[] opersY = new int[n];
+//        int[] opersZ = new int[n];
+//
+//
+//    }
 
 }
